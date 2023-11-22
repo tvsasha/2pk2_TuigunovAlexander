b150a4a68af415be842bd79e6e30ee342d4a4d1b
Александр Туйгунов <tvsasha2006@gmail.com>
2023-11-22 20:47:17 +0500

    добавлена 10 практика

--- PZ_10/Program.cs
+++ PZ_10/Program.cs
@@ -6,8 +6,8 @@
         {
             Console.WriteLine("Введите текст: ");     
             string input = Console.ReadLine();
-            var charCount = new Dictionary<char, int>();
-            foreach (char c in input)
+            var charCount = new Dictionary<char, int>();          //создание словаря
+            foreach (char c in input)                             //прохождение по всем символам
             {
                 if (char.IsLetter(c))
                 {
@@ -21,7 +21,7 @@
                     }
                 }
             }
-            var letter = charCount.OrderByDescending(x => x.Value).FirstOrDefault().Key;
+            var letter = charCount.OrderByDescending(x => x.Value).FirstOrDefault().Key; //нахождение наиболее часто встречающегося символа 
             var count = charCount[letter];
             Console.WriteLine($"Наиболее часто встречающийся символ: {letter}");
             Console.WriteLine($"Количество: {count}");
