858e648db6a0e0c9a3ae58f3e671fbdfb441ada4
Александр Туйгунов <tvsasha2006@gmail.com>
2023-11-22 20:40:09 +0500

    добавлена 10 практика

---
+++ PZ_10/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст: ");     
+            string input = Console.ReadLine();
+            var charCount = new Dictionary<char, int>();
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (charCount.ContainsKey(c))
+                    {
+                        charCount[c]++;
+                    }
+                    else
+                    {
+                        charCount[c] = 1;
+                    }
+                }
+            }
+            var letter = charCount.OrderByDescending(x => x.Value).FirstOrDefault().Key;
+            var count = charCount[letter];
+            Console.WriteLine($"Наиболее часто встречающийся символ: {letter}");
+            Console.WriteLine($"Количество: {count}");
+        }
+    }
+    
+}
