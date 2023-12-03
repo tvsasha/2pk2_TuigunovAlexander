b3f1a75b22dd79d5edcf64e5bc54f40c1c58ad3c
Александр Туйгунов <tvsasha2006@gmail.com>
2023-12-03 14:45:00 +0500

    добавлена 14 практика

--- PZ_14/Program.cs
+++ PZ_14/Program.cs
@@ -3,13 +3,10 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            
-            
-           
+        {        
             try
             {
-                using (FileStream f1Stream = new FileStream("f1.txt", FileMode.Open, FileAccess.Read))
+                using (FileStream f1Stream = new FileStream("f1.txt", FileMode.Open, FileAccess.Read))       // Открытие файловых потоков для чтения файлов f1.txt и f2.txt
                 using (FileStream f2Stream = new FileStream("f2.txt", FileMode.Open, FileAccess.Read))
                 using (StreamReader f1Reader = new StreamReader("f1.txt"))
                 using (StreamReader f2Reader = new StreamReader("f2.txt"))
@@ -17,12 +14,12 @@
                 {
                     string lineF1;
                     string lineF2;
-                    while ((lineF1 = f1Reader.ReadLine()) != null && (lineF2 = f2Reader.ReadLine()) != null)
+                    while ((lineF1 = f1Reader.ReadLine()) != null && (lineF2 = f2Reader.ReadLine()) != null)        // Чтение строк из файлов f1.txt и f2.txt и запись их в файл f3.txt
                     {
                         writer.WriteLine(lineF1);
                         writer.WriteLine(lineF2);
                     }
-                    while ((lineF1 = f1Reader.ReadLine()) != null)
+                    while ((lineF1 = f1Reader.ReadLine()) != null)                                             // Если в файлах остались строки, записываем их в файл f3.txt
                     {
                         writer.WriteLine(lineF1);
                     }
